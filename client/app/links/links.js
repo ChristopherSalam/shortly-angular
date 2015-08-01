dad77a4250517180f613ed726d259da8cc28c29d
ChristopherSalam <christopher.salam@gmail.com>
2015-08-01 12:24:46 -0700

    Styles added. Iframe added, then removed.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,8 +1,11 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $sce) {
 
   $scope.data = {};
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  }
 
   $scope.getLinks = function(){
     Links.links().then(function(data){
